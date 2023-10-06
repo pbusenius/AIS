8492ff5a059b7315c3cc20c3f71edea96042600e
pbusenius <14203989+pbusenius@users.noreply.github.com>
2023-10-06 15:25:12 +0200

    added first routes

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,8 +1,24 @@
+import { useState } from "react";
+import Live from "./scenes/live";
+import DDPI from "./scenes/ddpi";
+import Kritis from "./scenes/kritis";
+import VOI from "./scenes/voi";
+import ROI from "./scenes/roi";
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="app">
+      <Sidebar isSidebar={isSidebar}/>
+      <main className="content">
+        <Topbar setIsSidebar={setIsSidebar}/>
+        <Routes>
+          <Route path="/live" element={<Live/>}/>
+          <Route path="/kritis" element={<Kritis/>}/>
+          <Route path="/ddpi" element={<DDPI/>}/>
+          <Route path="/roi" element={<ROI/>}/>
+          <Route path="/voi" element={<VOI/>}/>
+        </Routes>
+      </main>
     </div>
   );
 }
